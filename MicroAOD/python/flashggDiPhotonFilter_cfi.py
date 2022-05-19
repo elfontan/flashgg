6b4704fa9a8f7979f45643e9039c71014def7a65
elfontan <elisa.fontanesi@cern.ch>
2022-05-19 14:22:54 +0200

    Fix for the photon cuts (both at MicroAOD and preselection level)

--- MicroAOD/python/flashggDiPhotonFilter_cfi.py
+++ MicroAOD/python/flashggDiPhotonFilter_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 diPhotonSelector = cms.EDFilter("FLASHggDiPhotonSelector",
                                 src = cms.InputTag("flashggDiPhotons"),
-                                cut = cms.string("leadingPhoton().pt > 0. && subLeadingPhoton().pt > 0.")
+                                cut = cms.string("leadingPhoton().pt > 25. && subLeadingPhoton().pt > 15.")
                                 )
 
 diPhotonFilter = cms.EDFilter("CandViewCountFilter",
--- Taggers/python/flashggPreselectedDiPhotons_cfi.py
+++ Taggers/python/flashggPreselectedDiPhotons_cfi.py
@@ -1,77 +1,60 @@
 import FWCore.ParameterSet.Config as cms
 
-#phoEffArea=cms.PSet( var=cms.string("abs(superCluster.eta)"), bins=cms.vdouble(0.,0.9,1.5,2,2.2,3), vals=cms.vdouble(0.16544,0.16544,0.13212,0.13212,0.13212) )
-phoEffArea=cms.PSet( var=cms.string("abs(superCluster.eta)"), bins=cms.vdouble(0.0,1.0,1.479,2.0,2.2,2.3,2.4,5.0), vals=cms.vdouble(0.124,0.109,0.063,0.078,0.100,0.115,0.137) )
-
+phoEffArea=cms.PSet( var=cms.string("abs(superCluster.eta)"), bins=cms.vdouble(0.,0.9,1.5,2,2.2,3), vals=cms.vdouble(0.16544,0.16544,0.13212,0.13212,0.13212) )
 
 rediscoveryHLTvariables = cms.vstring(
     "pfPhoIso03", 
     "trkSumPtHollowConeDR03",
     "full5x5_sigmaIetaIeta",
     "full5x5_r9",
-#    "passElectronVeto"
-    "hasPixelSeed"  ##PSV
+    "passElectronVeto"
     )
 
 #cuts to mimic category trigger cuts
 rediscoveryHLTcutsV1 = cms.VPSet(
     cms.PSet(cut=cms.string("isEB && full5x5_r9>0.85"), ##EB high R9
              selection = cms.VPSet(
-            cms.PSet(max=cms.string("4.0"),
+            cms.PSet(max=cms.string("100000.0"), 
                      rhocorr=phoEffArea,
                      ),
-            cms.PSet(max=cms.string("6.0")),
-            cms.PSet(max=cms.string("0.015")),
+            cms.PSet(max=cms.string("100000.0")),
+            cms.PSet(max=cms.string("100000.0")),
             cms.PSet(min=cms.string("0.5")),
-            cms.PSet(max=cms.string("0.5"))
+            cms.PSet(min=cms.string("0.5"))
             ),
              ),
     
     cms.PSet(cut=cms.string("isEE && full5x5_r9>0.90"),  ##EE high R9
              selection = cms.VPSet(
-            cms.PSet(max=cms.string("4.0"),
+            cms.PSet(max=cms.string("100000.0"), 
                      rhocorr=phoEffArea,
                      ),
-            cms.PSet(max=cms.string("6.0")),
-            cms.PSet(max=cms.string("0.035")),
+            cms.PSet(max=cms.string("100000.0")),
+            cms.PSet(max=cms.string("100000.0")),
             cms.PSet(min=cms.string("0.8")),
-            cms.PSet(max=cms.string("0.5"))
+            cms.PSet(min=cms.string("0.5"))
             ),
              ),
     cms.PSet(cut=cms.string("isEB && full5x5_r9<=0.85"),  #EB low R9
              selection = cms.VPSet(
-            cms.PSet(max=cms.string("4.0"),
+            cms.PSet(max=cms.string("4.0"), 
                      rhocorr=phoEffArea,
                      ),
             cms.PSet(max=cms.string("6.0")),
             cms.PSet(max=cms.string("0.015")),
             cms.PSet(min=cms.string("0.5")),
-            cms.PSet(max=cms.string("0.5"))
+            cms.PSet(min=cms.string("0.5"))
             ),       
              ),       
     cms.PSet(cut=cms.string("isEE && full5x5_r9<=0.90"),  ##EE low R9
              selection = cms.VPSet(
-            cms.PSet(max=cms.string("4.0"),
+            cms.PSet(max=cms.string("4.0"), 
                      rhocorr=phoEffArea,
                      ),
             cms.PSet(max=cms.string("6.0")),
             cms.PSet(max=cms.string("0.035")),
             cms.PSet(min=cms.string("0.8")),
-            cms.PSet(max=cms.string("0.5"))
-            ),
-             )
-    )
-
-rediscoveryNocutsV1 = cms.VPSet(
-    cms.PSet(cut=cms.string("full5x5_r9>=0.0"), ##no cuts
-             selection = cms.VPSet(
-            cms.PSet(max=cms.string("100000.0"),
-                     rhocorr=phoEffArea,
-                     ),
-            cms.PSet(max=cms.string("100000.0")),
-            cms.PSet(max=cms.string("100000.0")),
-            cms.PSet(min=cms.string("0.0")),
-            cms.PSet(max=cms.string("2.0"))
+            cms.PSet(min=cms.string("0.5"))
             ),
              )
     )
@@ -83,32 +66,18 @@ flashggPreselectedDiPhotons = cms.EDFilter(
     src = cms.InputTag(""),
     rho = cms.InputTag("fixedGridRhoAll"),
     cut = cms.string(
-     #   "    (leadingPhoton.full5x5_r9>0.8||leadingPhoton.egChargedHadronIso<20||leadingPhoton.egChargedHadronIso/leadingPhoton.pt<0.3)"
-     #   " && (subLeadingPhoton.full5x5_r9>0.8||subLeadingPhoton.egChargedHadronIso<20||subLeadingPhoton.egChargedHadronIso/subLeadingPhoton.pt<0.3)"
-     #   " && (leadingPhoton.hadronicOverEm < 0.08 && subLeadingPhoton.hadronicOverEm < 0.08)"
-# 
-     #   " && (abs(leadingPhoton.superCluster.eta) < 2.5 && abs(subLeadingPhoton.superCluster.eta) < 2.5)"
-     #   " && (abs(leadingPhoton.superCluster.eta) < 1.4442 || abs(leadingPhoton.superCluster.eta) > 1.566)"
-     #   " && (abs(subLeadingPhoton.superCluster.eta) < 1.4442 || abs(subLeadingPhoton.superCluster.eta) > 1.566)"
-     #  " && (leadingPhoton.pt >30.0 && subLeadingPhoton.pt > 18.0)"
-       "mass > 0"
-#
-     #  " && ( (abs(leadingPhoton.superCluster.eta) < 1.4442 && abs(subLeadingPhoton.superCluster.eta) < 1.4442 && (leadingPhoton.full5x5_r9 > 0.5 && subLeadingPhoton.full5x5_r9 > 0.5) )" #EB-EB : both photon R9>0.5
-     # "   || ( abs(leadingPhoton.superCluster.eta) < 1.4442 && abs(subLeadingPhoton.superCluster.eta) > 1.566 && (leadingPhoton.full5x5_r9 > 0.5 && subLeadingPhoton.full5x5_r9 > 0.9) )" #EB-EE : EB R9>0.5 and EE R9>0.9
-     #    "   || ( abs(leadingPhoton.superCluster.eta) > 1.566 &&  abs(subLeadingPhoton.superCluster.eta) < 1.4442 && (leadingPhoton.full5x5_r9 > 0.9 && subLeadingPhoton.full5x5_r9 > 0.5) )" #EE-EB: EE R9>0.9 and EB R9>0.5
-     #   "   || ( abs(leadingPhoton.superCluster.eta) > 1.566 && abs(subLeadingPhoton.superCluster.eta) > 1.566 && (leadingPhoton.full5x5_r9 > 0.9 && subLeadingPhoton.full5x5_r9 > 0.9) ) )" #EE-EE: both photon R9>0.9
-#
-     #   " && (leadingPhoton.pt > 0.47*mass && subLeadingPhoton.pt > 0.28*mass)"  #Scaled pTs
-     #   " && (!leadingPhoton.hasPixelSeed && !subLeadingPhoton.hasPixelSeed)"  #PSV e-veto again
-#
-     #   " && (leadPhotonId > -0.9 && subLeadPhotonId > -0.9)"
-#
+        "    (leadingPhoton.full5x5_r9>0.8||leadingPhoton.egChargedHadronIso<20||leadingPhoton.egChargedHadronIso/leadingPhoton.pt<0.3)"
+        " && (subLeadingPhoton.full5x5_r9>0.8||subLeadingPhoton.egChargedHadronIso<20||subLeadingPhoton.egChargedHadronIso/subLeadingPhoton.pt<0.3)"
+        " && (leadingPhoton.hadronicOverEm < 0.08 && subLeadingPhoton.hadronicOverEm < 0.08)"
+        " && (leadingPhoton.pt >35.0 && subLeadingPhoton.pt > 25.0)"
+        " && (abs(leadingPhoton.superCluster.eta) < 2.5 && abs(subLeadingPhoton.superCluster.eta) < 2.5)"
+        " && (abs(leadingPhoton.superCluster.eta) < 1.4442 || abs(leadingPhoton.superCluster.eta) > 1.566)"
+        " && (abs(subLeadingPhoton.superCluster.eta) < 1.4442 || abs(subLeadingPhoton.superCluster.eta) > 1.566)"
+        " && (leadPhotonId > -0.9 && subLeadPhotonId > -0.9)"
 #        " && (leadingPhoton.pt > mass/3. && subLeadingPhoton.pt > mass/4.)"
 #        " && (leadingPhoton.passElectronVeto) && (subLeadingPhoton.passElectronVeto)"
         ),
     variables = rediscoveryHLTvariables,
     categories = rediscoveryHLTcutsV1
-    #categories = rediscoveryNocutsV1
-
     )
 
