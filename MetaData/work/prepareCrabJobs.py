9982557fcd91029e88f6f35a8fc1d3aa83046e9e
elfontan <elisa.fontanesi@cern.ch>
2022-06-23 16:08:15 +0200

    Updates for the 90 GeV sample and diphoton preselection configuration

--- MetaData/work/prepareCrabJobs.py
+++ MetaData/work/prepareCrabJobs.py
@@ -313,8 +313,10 @@ if options.createCrabConfig:
             ("RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1", "Autumn18"),
             ("RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3", "Summer16"),
             ("RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15", "Fall18"),
-            ("RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1", "Summer20UL18")
-        ]
+            ("RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1", "Summer20UL18"),
+            ("RunIISummer20UL18MiniAODv2-minus10percentMaterial_106X_upgrade2018_realistic_v16_L1v1-v1", "Summer20UL18"),
+            ("RunIISummer20UL18MiniAODv2-m10Mat_106X_upgrade2018_realistic_v16_L1v1-v1", "Summer20UL18")
+        ]     
 
         for orig, replacement in replacements:
             if len(jobname) <= 97:
